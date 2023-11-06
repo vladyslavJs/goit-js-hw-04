cf8e38b9fa7b531cf091f858bd4de36a03c7a23e
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-06 18:08:58 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,21 @@
-// Задача 3. Фільтрація масиву чисел
+// Задача 3. Профіль гравця
 
-// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
+// Об'єкт profile описує профіль користувача на ігровій платформі. У його властивостях зберігається ім'я профілю username та кількість активних годин playTime, проведених у грі.
+// Доповни об'єкт profile методами для роботи з його властивостями.
 
-// Усередині функції:
+// Метод changeUsername(newName) повинен приймати рядок (нове ім'я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім'я профілю, а <amount> — кількість ігрових годин.
 
-// Створи порожній масив, у який будеш додавати підходящі числа.
-// Використай цикл для ітерації кожного елемента масиву numbers.
-// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
-// Поверни свій новий масив з підходящими числами як результат.
-
-function filterArray(numbers, value) {
-    const newArrayNumbers = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            newArrayNumbers.push(numbers[i]);
-        }
-    }
-    return newArrayNumbers;    
+function name(params) {
+   
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4)); 
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38)); 
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
