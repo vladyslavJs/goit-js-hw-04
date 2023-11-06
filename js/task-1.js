7e7c9a1092407aa5ac3fe25d55facfb8333a5c63
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-06 18:08:50 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,27 @@
-// Задача 1. Генератор slug
+// Задача 1. Пакування товарів
 
-// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
+// Функція оголошує два параметри:
+// products — об'єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об'єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
-// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-// Усі символи slug повинні бути в нижньому регістрі.
-// Усі слова slug повинні бути розділені тире.\
-
-function slugify(title) {
-    const slugPart = title.toLowerCase().trim();
-    const slug = slugPart.split(' ');
-
-    return slug.join('-');
+function isEnoughCapacity(products, containerSize) {
+    
 }
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
